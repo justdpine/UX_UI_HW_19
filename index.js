f2b6313b165a6de5e67cc0b0ab21a3f66d106728
justdpine <93888647+justdpine@users.noreply.github.com>
2022-05-03 17:14:35 -0700

    HW19 - jquery event listeners, UI interactions

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('header').first().delay(400).animate({ 'opacity': '1' }, 800);
+    $('.welcome').last().delay(1000).animate({ 'opacity': '1' }, 1400);
+    $('.i-am').delay(2000).animate({ 'opacity': '1' }, 1400);
+    $('.skill').delay(5000).animate({ 'opacity': '1' }, 1400);
+    $('.bottom-landing').delay(7000).animate({ 'opacity': '1' }, 1200);
+});
